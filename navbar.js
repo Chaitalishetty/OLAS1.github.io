64a3540c2696f835445a8d26eca5438b4e69672c
Chaitalish02 <chaitalishetty261@gmail.com>
2021-08-28 15:20:25 +0530

    Added home,gallery

---
+++ navbar.js
@@ -0,0 +1,11 @@
+navbar=()=>{
+    document.querySelector("#navbar").innerHTML=` <nav>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="features.html">Features</a></li>
+        <li><a href="gallery.html">Gallery</a></li>
+    </ul>
+</nav>`;
+}
