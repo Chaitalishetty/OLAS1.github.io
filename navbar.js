931950abbedd37c98eb82eaf0d2f4f50f9d95bc2
Chaitalish02 <chaitalishetty261@gmail.com>
2021-08-28 21:00:11 +0530

    navbar

--- navbar.js
+++ navbar.js
@@ -1,6 +1,9 @@
 navbar=()=>{
     document.querySelector("#navbar").innerHTML=` <nav>
-    <ul>
+    <span class="navbar-toggle" id="nav_ham">
+    <i class="fa fa-bars" style="color:white" font-size:"30px"></i>
+    </span>
+    <ul class="main_nav" id="nav_list">
         <li><a href="index.html">Home</a></li>
         <li><a href="about.html">About</a></li>
         <li><a href="contact.html">Contact</a></li>
@@ -8,14 +11,10 @@ navbar=()=>{
         <li><a href="gallery.html">Gallery</a></li>
     </ul>
 </nav>`;
-
+let mainNav = document.getElementById('nav_list');
+let navBarToggle = document.getElementById('nav_ham');
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('open');});
 }
     
-function closeNav() {
-    const mediaQuery=window.matchMedia("(max-width:870px)");
-    if (mediaQuery.matches){
-      document.querySelector("ul").style.display = "none";
-      // document.querySelector(".dashboard_container").style.marginLeft= "0";
-      // document.querySelector(".dashboard_container").style.width = "100vw";
-    }
-  }
